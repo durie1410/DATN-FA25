1c9eaa9ff03625f8e0c4af2677af3b422f604589
Tung Nguyen <tungtam262005@gmailcom>
2025-12-17 01:37:19 +0700

    fix api

--- routes/api.php
+++ routes/api.php
@@ -45,6 +45,7 @@ Route::get('/books', [BookApiController::class, 'index']);
 Route::get('/books/{id}', [BookApiController::class, 'show']);
 Route::get('/books/featured', [BookApiController::class, 'featured']);
 Route::get('/categories', [BookApiController::class, 'categories']);
+Route::get('/categories', [BookApiController::class, 'categoriess']);
 
 // Departments API
 Route::get('/departments', [DepartmentApiController::class, 'index']);
