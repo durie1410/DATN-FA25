67a1d65fec762cd2d59bcae10bf5ee132d544362
Tung Nguyen <tungtam262005@gmailcom>
2026-01-02 19:54:41 +0700

    model orderItem

--- app/Models/OrderItem.php
+++ app/Models/OrderItem.php
@@ -18,12 +18,6 @@ class OrderItem extends Model
         'price',
         'quantity',
         'total_price',
-        'total_price',
-        'total_price',
-        'total_price',
-        'total_price',
-        'total_price',
-        'total_price',
     ];
 
     protected $casts = [
